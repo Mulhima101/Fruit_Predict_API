0fa27ef452014e0d2eff894f666e4016432b35f9
Jawahirm101 <jawahirm821@gmail.com>
2024-09-10 22:51:19 +0530

    new update

--- app.py
+++ app.py
@@ -1,115 +1,88 @@
-import streamlit as st
-import pandas as pd
+from fastapi import FastAPI, File, UploadFile
+import tensorflow as tf
+from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing import image
+from tensorflow.keras.layers import Lambda
+from tensorflow import keras
 import numpy as np
 from PIL import Image
-import time
-import altair as alt
 import os
+import io
 import requests
 
-process_image_pix = 162
+app = FastAPI()
 
-def download_new_version_of_model():
-    # URL to download from
-    url = "https://drive.usercontent.google.com/download?id=10SF_y4Gb-7xSDTiBAaWgp4KtRdQd7Lg4&export=download&authuser=0&confirm=t&uuid=ff412ef3-252f-44ca-8b26-8ed400be617d&at=AO7h07fQT85gkQj02wvMRcb7ztVq:1725860542562"
-    
-    # Define folder and file paths
-    folder_name = "fruit-360_model"
-    file_name = "fruit-360_model.npy"
+# # Download model function
+# def download_new_version_of_model():
+#     url = "https://drive.usercontent.google.com/download?id=11jx-8NS5sU9tqFVgML9VOcpMaX_2ZaWl&export=download&authuser=0&confirm=t&uuid=a8c7024d-83f1-49c3-b723-6a06189f5a1c&at=AO7h07fqdlUwCe7UJ3dZBpprFe5h:1725951295439"
+#     folder_name = "fruit-360_model"
+#     file_name = "fruit-360_model.keras"
 
-    # Create the folder if it doesn't exist
-    if not os.path.exists(folder_name):
-        os.makedirs(folder_name)
+#     if not os.path.exists(folder_name):
+#         os.makedirs(folder_name)
 
-    # Full path to save the file
-    file_path = os.path.join(folder_name, file_name)
+#     file_path = os.path.join(folder_name, file_name)
+#     response = requests.get(url, stream=True)
+#     total_size = int(response.headers.get('content-length', 0))
+#     block_size = 1024
+#     downloaded_size = 0
 
-    # Streamlit progress bar and status text
-    progress_bar = st.progress(0)
-    status_text = st.empty()
+#     if response.status_code == 200:
+#         with open(file_path, 'wb') as f:
+#             for data in response.iter_content(block_size):
+#                 f.write(data)
+#                 downloaded_size += len(data)
+#                 progress = int(downloaded_size / total_size * 100)
+#                 print(f"\rDownloaded {round(downloaded_size/1024/1024,2)} MB of {round(total_size/1024/1024,2)} MB ({progress}%)", end=" ")
 
-    # Download the file with streaming
-    response = requests.get(url, stream=True)
-    total_size = int(response.headers.get('content-length', 0))  # Total size in bytes
-    block_size = 1024  # 1 Kilobyte
-    downloaded_size = 0  # Track the downloaded size
+# download_new_version_of_model()
 
-    if response.status_code == 200:
-        with open(file_path, 'wb') as f:
-            for data in response.iter_content(block_size):
-                f.write(data)
-                downloaded_size += len(data)
-                # Update progress bar and status text
-                progress = int(downloaded_size / total_size * 100)
-                progress_bar.progress(progress / 100)
-                status_text.text(f"Downloaded {round(downloaded_size/1024/1024,2)} MB of {round(total_size/1024/1024,2)} MB ({progress}%)")
+# Custom Lambda function for converting to HSV and Grayscale
+def convert_to_hsv_and_grayscale(x):
+    hsv = tf.image.rgb_to_hsv(x)
+    grayscale = tf.image.rgb_to_grayscale(x)
+    return tf.concat([hsv, grayscale], axis=-1)
 
-        # Clear progress bar and status text after download is complete
-        progress_bar.empty()
-        status_text.empty()
-        st.success(f"File downloaded successfully and saved to {file_path}")
-    else:
-        progress_bar.empty()  # Ensure progress bar disappears on failure too
-        st.error("Failed to download the file")
+# Register the custom function with TensorFlow
+@tf.keras.utils.register_keras_serializable()
+def custom_lambda(x):
+    return convert_to_hsv_and_grayscale(x)
 
+# # Use the correct decorator for registering custom objects
+# @tf.keras.utils.register_keras_serializable()
+# def convert_to_hsv_and_grayscale(x):
+#     hsv = tf.image.rgb_to_hsv(x)
+#     gray = tf.image.rgb_to_grayscale(x)
+#     rez = tf.concat([hsv, gray], axis=-1)
+#     return rez
 
-# Title and Description
-st.title("Food Image Classification App")
-st.write("Upload an image to get the food classification")
+# Define output shape for Lambda layer
+def custom_lambda_output_shape(input_shape):
+    return input_shape[:-1] + (4,)
 
-# Sidebar for user input
-st.sidebar.title("User Input")
-uploaded_file = st.sidebar.file_uploader("Upload an image file", type=["png", "jpg", "jpeg"])
+# Load the .keras model with the custom Lambda layer
+model = load_model("./fruit-360_model/fruit-360_model.keras", custom_objects={
+    'convert_to_hsv_and_grayscale': convert_to_hsv_and_grayscale,
+    'Lambda': Lambda(convert_to_hsv_and_grayscale, output_shape=custom_lambda_output_shape),
+    'custom_lambda': custom_lambda
+})
 
-# Class index to food name mapping
+# model = load_model("./fruit-360_model/fruit-360_model.keras")
+
+# # Example class names (adjust based on your dataset)
 class_names = ['Guava 1', 'Physalis 1', 'Pineapple Mini 1', 'Potato White 1', 'Zucchini 1', 'Grapefruit White 1', 'Tangelo 1', 'Lemon 1', 'Ginger Root 1', 'Tamarillo 1', 'Melon Piel de Sapo 1', 'Tomato 4', 'Strawberry 1', 'Peach 2', 'Grape White 3', 'Pomelo Sweetie 1', 'Avocado ripe 1', 'Pepino 1', 'Carambula 1', 'Avocado 1', 'Cherry 1', 'Pear Kaiser 1', 'Quince 1', 'Apricot 1', 'Peach 1', 'Mulberry 1', 'Cactus fruit 1', 'Tomato Maroon 1', 'Apple hit 1', 'Pitahaya Red 1', 'Pear Red 1', 'Apple Braeburn 1', 'Cucumber 1', 'Banana Red 1', 'Onion Red 1', 'Apple Red Yellow 2', 'Physalis with Husk 1', 'Eggplant long 1', 'Apple 6', 'Banana 1', 'Redcurrant 1', 'Plum 1', 'Nectarine 1', 'Orange 1', 'Potato Sweet 1', 'Grape White 2', 'Cherry Wax Yellow 1', 'Cucumber Ripe 2', 'Lemon Meyer 1', 'Cherry Rainier 1', 'Cabbage white 1', 'Blueberry 1', 'Hazelnut 1', 'Dates 1', 'Potato Red 1', 'Mango 1', 'Corn Husk 1', 'Onion White 1', 'Lychee 1', 'Mangostan 1', 'Rambutan 1', 'Cherry 2', 'Pepper Yellow 1', 'Cantaloupe 2', 'Grape White 4', 'Tomato Heart 1', 'Cherry Wax Black 1', 'Apple Red 1', 'Apple Red 2', 'Granadilla 1', 'Cucumber 3', 'Apple Red Delicious 1', 'Kaki 1', 'Clementine 1', 'Tomato Cherry Red 1', 'Pear 1', 'Passion Fruit 1', 'Chestnut 1', 'Nut Pecan 1', 'Pear Forelle 1', 'Apple Golden 1', 'Beetroot 1', 'Pepper Orange 1', 'Mango Red 1', 'Potato Red Washed 1', 'Pear 3', 'Tomato 1', 'Carrot 1', 'Limes 1', 'Kiwi 1', 'Peach Flat 1', 'Apple Pink Lady 1', 'Mandarine 1', 'Cauliflower 1', 'Zucchini dark 1', 'Cucumber Ripe 1', 'Papaya 1', 'Onion Red Peeled 1', 'Watermelon 1', 'Kohlrabi 1', 'Grape Pink 1', 'Nut Forest 1', 'Pear 2', 'Fig 1', 'Tomato not Ripened 1', 'Pineapple 1', 'Tomato Yellow 1', 'Apple Golden 2', 'Pear Monster 1', 'Grape Blue 1', 'Grapefruit Pink 1', 'Strawberry Wedge 1', 'Salak 1', 'Kumquats 1', 'Huckleberry 1', 'Pomegranate 1', 'Cantaloupe 1', 'Pepper Red 1', 'Banana Lady Finger 1', 'Corn 1', 'Pear Williams 1', 'Plum 2', 'Nectarine Flat 1', 'Raspberry 1', 'Apple Golden 3', 'Maracuja 1', 'Walnut 1', 'Plum 3', 'Apple Crimson Snow 1', 'Apple Granny Smith 1', 'Pear Abate 1', 'Tomato 2', 'Apple Red Yellow 1', 'Pear Stone 1', 'Cocos 1', 'Apple Red 3', 'Pepper Green 1', 'Grape White 1', 'Cherry Wax Red 1', 'Eggplant 1', 'Tomato 3']
 
-# Function to perform convolution
-def conv2d(image, kernel, bias):
-    output_height = image.shape[0] - kernel.shape[0] + 1
-    output_width = image.shape[1] - kernel.shape[1] + 1
-    output_channels = kernel.shape[3]
-    
-    output = np.zeros((output_height, output_width, output_channels))
-    
-    for k in range(output_channels):
-        for i in range(output_height):
-            for j in range(output_width):
-                image_patch = image[i:i + kernel.shape[0], j:j + kernel.shape[1], :]
-                kernel_slice = kernel[:, :, :, k]
-                if image_patch.shape[2] != kernel_slice.shape[2]:
-                    raise ValueError(f"Mismatch in input channels: image has {image_patch.shape[2]}, kernel has {kernel_slice.shape[2]}")
-                output[i, j, k] = np.sum(image_patch * kernel_slice) + bias[k]
-    
-    return output
+# # Preprocessing function for incoming images
+# def preprocess_image(image_data: Image.Image, target_size: tuple):
+#     image_data = image_data.resize(target_size)
+#     img_array = image.img_to_array(image_data)
+#     img_array = np.expand_dims(img_array, axis=0)  # Expand dimensions to fit model input
+#     img_array /= 255.0  # Normalize the image data
+#     return img_array
 
-# Function to perform max pooling
-def max_pool(image, pool_size):
-    output = np.zeros((image.shape[0] // pool_size[0], image.shape[1] // pool_size[1], image.shape[2]))
-    for c in range(image.shape[2]):
-        for i in range(output.shape[0]):
-            for j in range(output.shape[1]):
-                output[i, j, c] = np.max(image[i * pool_size[0]:(i + 1) * pool_size[0], j * pool_size[1]:(j + 1) * pool_size[1], c])
-    return output
-
-# Function to apply ReLU activation
-def relu(x):
-    return np.maximum(0, x)
-
-# Function to apply softmax activation
-def softmax(x):
-    e_x = np.exp(x - np.max(x))
-    return e_x / e_x.sum(axis=-1, keepdims=True)
-
-def add_alpha_channel(image_data):
-    # Create an alpha channel with the same height and width as the image_data
-    height, width, _ = image_data.shape
-    alpha_channel = np.ones((height, width, 1))
-    image_data_with_alpha = np.concatenate((image_data, alpha_channel), axis=-1)
-    return image_data_with_alpha
-
-def crop_center(image, new_size):
+def crop_center(image):
     # Crop the image to a square of size new_size, centered.
+    new_size = min(image.size)
     width, height = image.size
     left = (width - new_size) / 2
     top = (height - new_size) / 2
@@ -117,122 +90,75 @@ def crop_center(image, new_size):
     bottom = (height + new_size) / 2
     return image.crop((left, top, right, bottom))
 
-def flatten(pool2):
-    # Ensure you are flattening the output from the last pooling layer correctly
-    return pool2.reshape(-1)
+def preprocess_image(img_data, target_size=(100, 100)):
+    img = crop_center(img_data)
+    img = img.resize(target_size)  # Resize to match model input
+    img_array = tf.keras.utils.array_to_img(img)  # Convert image to array
+    img_array = tf.expand_dims(img_array, axis=0)  # Add batch dimension
+    img_array /= 255 # Normalize image
+    return img_array
 
-if uploaded_file is not None:
-    download_new_version_of_model()
-    image = Image.open(uploaded_file)
-    st.image(image, caption='Uploaded Image', use_column_width=True)
+# # Function to load the model, predict, and output the class label
+# def load_and_predict_image(image_data, model_out_dir, labels):
+#     # Load model weights
+#     model = keras.models.load_model(model_out_dir + "/fruit-360_model.keras",
+#                                     custom_objects={"convert_to_hsv_and_grayscale": convert_to_hsv_and_grayscale})
 
-    # Process The Uploaded Image to fit for the model
-    image = image.convert('RGB')
+#     # Normalize the image array
+#     img_array = preprocess_image(image_data)
+#     # img_array = img_array / 255.0  # Normalize pixel values to [0, 1]
 
-    min_dim = min(image.size)
-    cropped_image = crop_center(image, min_dim)
+#     # Perform prediction
+#     predictions = model.predict(img_array)
+    
+#     # Get the predicted class index with highest probability
+#     score= tf.nn.softmax(predictions)
 
-    resized_image = cropped_image.resize((process_image_pix, process_image_pix))
-    image_data = np.array(resized_image) / 255.0
-    image_data = add_alpha_channel(image_data)
-    image_data = image_data.reshape(1, process_image_pix, process_image_pix, 4)
+#     # Check if predicted_class_index is in the range of labels
+#     if score >= len(labels):
+#         return {"error": "Predicted class index is out of range."}
+    
+#     predicted_label = labels[np.argmax(score)]
+#     print("lable: ", predicted_label)
+    
+#     return predicted_label
 
-    st.image(resized_image, caption='Edited Image (100x100)', use_column_width=True)
+@app.post("/")
+async def predict(file: UploadFile = File(...)):
+    try:
+        # # Read and process the uploaded image
+        image_data = await file.read()
+        image_data = Image.open(io.BytesIO(image_data)).convert('RGB')
 
-    # Load the pre-trained model weights
-    def load_model():
-        try:
-            weights_dict = np.load('./fruit-360_model/fruit-360_model.npy', allow_pickle=True).item()
-            return weights_dict
-        except Exception as e:
-            st.error(f"Error loading model: {e}")
-            return None
+        # result = load_and_predict_image(image_data, "./fruit-360_model", class_names)
+        
+        # Resize and preprocess the image
+        processed_image = preprocess_image(image_data, target_size=(100, 100))
 
-    model_weights = load_model()
+        # Make predictions using the loaded model
+        predictions = model.predict(processed_image)
 
-    if model_weights:
-        # Progress bar
-        progress_bar = st.progress(0)
-        status_text = st.empty()
+        # Debugging: Print the prediction results and their shape
+        print(f"Predictions: {predictions}")
+        print(f"Prediction shape: {predictions.shape}")
+        
+        predicted_class = np.argmax(predictions, axis=1)[0]
+        
+        # Debugging: Print the predicted class index
+        print(f"Predicted class index: {predicted_class}")
 
-        # Perform the forward pass with progress updates
-        def predict(image_data, model_weights):
-            # First convolutional layer
-            status_text.text("Processing: First Convolutional Layer...")
-            conv1 = conv2d(image_data[0], model_weights['conv1_kernel'], model_weights['conv1_bias'])
-            conv1 = relu(conv1)
-            pool1 = max_pool(conv1, (2, 2))
-            progress_bar.progress(25)
+        # Make sure the predicted class index is within bounds of class_names
+        if predicted_class < len(class_names):
+            predicted_class_name = class_names[predicted_class]
+        else:
+            return {"error": "Predicted class index is out of range."}
 
-            # Second convolutional layer
-            status_text.text("Processing: Second Convolutional Layer...")
-            conv2 = conv2d(pool1, model_weights['conv2_kernel'], model_weights['conv2_bias'])
-            conv2 = relu(conv2)
-            pool2 = max_pool(conv2, (2, 2))
-            progress_bar.progress(50)
+        # Return the prediction result
+        return {
+            # "predicted_class_index": int(confidence_scores),
+            "predicted_class_name": predicted_class_name,
+            # "confidence_scores": predictions.tolist()
+        }
 
-            # Third convolutional layer
-            status_text.text("Processing: Third Convolutional Layer...")
-            conv3 = conv2d(pool2, model_weights['conv3_kernel'], model_weights['conv3_bias'])
-            conv3 = relu(conv3)
-            pool3 = max_pool(conv3, (2, 2))
-            progress_bar.progress(65)
-
-            # Fourth convolutional layer
-            status_text.text("Processing: Fourth Convolutional Layer...")
-            conv4 = conv2d(pool3, model_weights['conv4_kernel'], model_weights['conv4_bias'])
-            conv4 = relu(conv4)
-            pool4 = max_pool(conv4, (2, 2))
-            progress_bar.progress(75)
-
-            print("Shape of pool4 before flattening:", pool4.shape)
-            # Flatten the output from the last pooling layer
-            status_text.text("Processing: Flattening Layer...")
-            flat = flatten(pool4)
-            progress_bar.progress(80)
-            print("Shape of pool4 after flattening:", flat.shape)
-
-            # Fully connected layer 1 (fcl1)
-            status_text.text("Processing: Fully Connected Layer 1...")
-            fcl1 = np.dot(flat, model_weights['fcl1_kernel']) + model_weights['fcl1_bias']
-            fcl1 = relu(fcl1)
-            progress_bar.progress(85)
-
-            # Fully connected layer 2 (fcl2)
-            status_text.text("Processing: Fully Connected Layer 2...")
-            fcl2 = np.dot(fcl1, model_weights['fcl2_kernel']) + model_weights['fcl2_bias']
-            fcl2 = relu(fcl2)
-            progress_bar.progress(90)
-
-            # Output layer (predictions)
-            status_text.text("Processing: Output Layer...")
-            predictions = np.dot(fcl2, model_weights['predictions_kernel']) + model_weights['predictions_bias']
-            output = softmax(predictions)
-            progress_bar.progress(100)
-
-            return output
-
-        predictions = predict(image_data, model_weights)
-        predicted_class = np.argmax(predictions)
-        predicted_food = class_names[predicted_class]
-
-        st.write(f"Predicted Class Index: {predicted_class}")
-        st.write(f"Predicted Food: {predicted_food}")
-
-        progress_bar.empty()
-        status_text.empty()
-    else:
-        st.error("Failed to load model weights.")
-
-slider_value = st.slider("Select a value", 0, 100)
-text_input = st.text_input("Enter some text")
-
-# Progress and status updates with delay
-progress_bar = st.progress(0)
-for i in range(100):
-    time.sleep(0.02)
-    progress_bar.progress(i + 1)
-
-data = pd.DataFrame(np.random.randn(100, 3), columns=['a', 'b', 'c'])
-chart = alt.Chart(data).mark_line().encode(x='a', y='b')
-st.altair_chart(chart)
+    except Exception as e:
+        return {"error": str(e)}
